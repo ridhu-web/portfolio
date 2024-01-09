bde72f512cb1f6fe59e64d3cd95efeba72de9780
ridhu-web <rkungu2@uic.edu>
2024-01-09 08:49:37 -0600

    git routing

--- src/components/skills/SkillSection.js
+++ src/components/skills/SkillSection.js
@@ -2,12 +2,20 @@ import React from "react";
 import SkillCard from "./Skillcard";
 import "./TabPanel.css";
 import { Margin } from "@mui/icons-material";
+import { info } from "../../info/Info";
 
 const SkillSection = ({ title, skills }) => {
   return (
     <div>
       <div className="d-flex flex-wrap justify-content-center">
-        <h4 className="panelText" style={{ marginLeft: "2rem" }}>
+        <h4
+          className="panelText"
+          style={{
+            marginLeft: "2rem",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+          }}
+        >
           {title}
         </h4>
         {skills.map((skill, index) => (
